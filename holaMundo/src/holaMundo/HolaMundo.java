0c3764e00ffb0dda037e48fd62019967482119dc
root <daniel-dezz@hotmail.com>
2021-03-10 13:35:44 +0100

    3 cambio desde consola

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -4,12 +4,13 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("hola daniel");
-		System.out.println("cambio desde consola")
+		System.out.println("cambio desde consola");
 		System.out.println("cambio desde eclipse");
 		System.out.println("2 cambio desde eclipse");
 		System.out.println("3 cambio desde eclipse");
 		System.out.println("4 cambio desde eclipse");
-
+		System.out.println("2 combio desde consola");
+		System.out.println("3 cambio desde consola");
 
 	}
 
