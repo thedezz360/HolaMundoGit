3598661f87e63a04990a259538bd45837461442e
root <daniel-dezz@hotmail.com>
2021-03-10 13:06:25 +0100

    un cambio mas desde eclipse

--- holaMundo/src/holaMundo/HolaMundo.java
+++ holaMundo/src/holaMundo/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("hola daniel");
 		System.out.println("cambio desde eclipse");
+		System.out.println("2 cambio desde eclipse");
 
 	}
 
